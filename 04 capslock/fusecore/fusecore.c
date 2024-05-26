316201b955260bb34044122e4cd023518f3e4046
orca-li <krylov.yar.igo@gmail.com>
2024-05-26 15:27:45 +0300

    пофиксил transmit

--- 04 capslock/fusecore/fusecore.c
+++ 04 capslock/fusecore/fusecore.c
@@ -11,11 +11,13 @@ void fusecore (void) {
 static void usart_init (void) {
     rcc_periph_clock_enable(AHBUS1, GPIOAEN);
     gpio_mode_setup(GPIOA, PIN2, GPIO_MODE_AF);
+    gpio_mode_setup(GPIOA, PIN3, GPIO_MODE_AF);
     gpio_af_setup(GPIOA, AFRLOW, PIN2, GPIO_AF07);
+    gpio_af_setup(GPIOA, AFRLOW, PIN3, GPIO_AF07);
 
     rcc_periph_clock_enable(APBUS1, USART2EN);
     usart_set_baudrate(USART2, 9600);
-    usart_set_mode(USART2, USART_MODE_TX);
+    usart_set_mode(USART2, USART_MODE_TX_RX);
     usart_enable(USART2);    
 }
 
