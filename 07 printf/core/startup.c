f5dcfaf89f1d1c91d7b2adc0c001fc592f708ee6
krylov-yi <krylov@kit-invest.ru>
2024-05-30 13:08:40 +0300

    исправлен баг с кучей из-за которого не работал printf

---
+++ 07 printf/core/startup.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+
+#include "bridge/memory.h"
+
+extern uint32_t _etext, _sdata, _edata, _sbss, _ebss;
+
+void main(void);
+
+void reset_handler(void){
+    uint32_t size_data = (uint32_t)&_edata - (uint32_t)&_sdata;
+    uint8_t *flash_data = (uint8_t*)&_etext;
+    uint8_t *sram_data = (uint8_t*)&_sdata;
+
+    for(uint32_t i = 0; i < size_data; i++)
+        sram_data[i] = flash_data[i];
+
+    uint32_t size_bss = (uint32_t)&_ebss - (uint32_t)&_sbss;
+    uint8_t *bss = (uint8_t*)&_sbss;
+
+    for(uint32_t i = 0; i < size_bss; i++)
+        bss[i] = 0;
+
+    main();
+}
---
+++ 07 printf/software/capslock.h
@@ -0,0 +1,8 @@
+#ifndef _SOFTWARE_CAPSLOCK_H
+#define _SOFTWARE_CAPSLOCK_H
+
+typedef char char_ct;
+
+void capslock(uint32_t channel);
+
+#endif /* _SOFTWARE_CAPSLOCK_H */
