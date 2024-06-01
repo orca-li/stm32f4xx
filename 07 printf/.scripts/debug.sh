ce7754aac828d3f4e2dc6a132871a3719e008169
orca-li <krylov.yar.igo@gmail.com>
2024-06-01 19:15:31 +0300

    рефакторинг

--- 07 printf/.scripts/debug.sh
+++ 07 printf/.scripts/debug.sh
@@ -5,12 +5,12 @@ set -e
 path="$bindir/$pname.elf > $dbgdir/$pname"
 
 #objdump
-action="$odump -d $path.S" # ассемблерный код для всех секций
+action="$odump -d $path.text" # ассемблерный код для всех секций
 todo
-action="$odump -h $path.ods" # вывод информации о всех секциях
-todo
-action="$odump -t $path.odc" # вывод таблицы символов
-todo
+# action="$odump -h $path.ods" # вывод информации о всех секциях
+# todo
+# action="$odump -t $path.odc" # вывод таблицы символов
+# todo
 
 #nm
 action="$ccmap $path.list" # вывод всех символов из файла объекта
@@ -25,9 +25,9 @@ action="$ccstr $path.str" # извлечение всех ASCII-строк из 
 todo
 
 #readelf
-action="$relf -h $path.hd" # вывод заголовка ELF файла
-todo
-action="$relf -S $path.rfs" # вывод информации о всех секциях
-todo
-action="$relf -s $path.rfc" # вывод информации о символах
+# action="$relf -h $path.hd" # вывод заголовка ELF файла
+# todo
+# action="$relf -S $path.rfs" # вывод информации о всех секциях
+# todo
+action="$relf -s $path.symtab" # вывод информации о символах
 todo
