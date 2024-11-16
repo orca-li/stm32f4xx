5f3d95a259509d753b089738c2de17a2701bc7c6
orca-li <krylov.yar.igo@gmail.com>
2024-11-17 01:16:01 +0300

    Update Submod.sh

--- 10_i2c/Submod.sh
+++ 10_i2c/Submod.sh
@@ -1,7 +1,7 @@
 rm -rf ./driver/CMSIS
 cd ./driver
 git clone https://github.com/ARM-software/CMSIS_5 CMSIS
-cd ./CMSIS
+cd ./CMSIS/Device
 mkdir ST
 cd ./ST
 git clone https://github.com/STMicroelectronics/cmsis_device_f4 STM32F4
